d3a6ea6b0c41f6c49c2c3751f2369ee8e133d08d
durasno <sashka.warner@gmail.com>
2019-03-30 16:58:56 -0700

    add tip calculator

--- scripts.js
+++ scripts.js
@@ -17,6 +17,32 @@ while ( i < 4 ) {
   i = i + 1;
 }*/
 
-for (let i = 1; i < 4; i = i + 1 ) {
-  $("#response").append("<br />" + i);
-}
+//request user input
+/*let userInput = prompt("What is your favorite number (less than 20)?", "Enter text here");
+
+//print first string
+//$("#response").html("This is the response div.");
+
+//print out loop
+for (let i = 1; i < userInput; i = i + 1 ) {
+  if(i % 2 == 0){
+    $("#response").append("<br />" + i + " is even");
+  }
+  else{
+    $("#response").append("<br />" + i + " is odd");
+  }
+}*/
+
+let tipCalculator;
+tipCalculator = function(total, tipRate){
+  // step 1:
+  let tipAmount;
+  tipAmount = tipRate * total;
+  // and step 2:
+  $("#response").html("Your tip is $" + tipAmount);
+};
+
+// Now call (or “execute”) the function, passing a
+// total of $50.00 and a tipRate of 20%:
+
+tipCalculator(50.00, 0.2);
