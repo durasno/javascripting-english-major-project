707bdcbefaf0dee1017710bc3687c5ae618e6d2b
durasno <sashka.warner@gmail.com>
2019-03-30 16:32:52 -0700

    add if

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 //alert("scripts.js has loaded!");
 //console.log("scripts.js has loaded!");
-$("#response").html("scripts.js has loaded!");
+//$("#response").html("scripts.js has loaded!");
+let userInput;
+userInput = "burrito";
+if ( userInput === "burrito" ) {
+  $("#response").html("Brilliant choice!");
+} else {
+  $("#response").html("Don’t you want a burrito?");
+}
