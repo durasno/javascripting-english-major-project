49b9a4f9956ee91617545d9f84ac079bba36cd54
durasno <sashka.warner@gmail.com>
2019-03-30 16:41:11 -0700

    change to for

--- scripts.js
+++ scripts.js
@@ -1,11 +1,22 @@
 //alert("scripts.js has loaded!");
 //console.log("scripts.js has loaded!");
 //$("#response").html("scripts.js has loaded!");
-let userInput;
+//let userInput;
 //userInput = "burrito";
-userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
+/*userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
 if ( userInput === "burrito" ) {
   $("#response").html("Brilliant choice!");
 } else {
   $("#response").html("Don’t you want a burrito?");
+}*/
+
+/*let i;
+i = 1;
+while ( i < 4 ) {
+  $("#response").append("<br />" + i);
+  i = i + 1;
+}*/
+
+for (let i = 1; i < 4; i = i + 1 ) {
+  $("#response").append("<br />" + i);
 }
