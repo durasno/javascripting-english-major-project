d07e1e23fe62037044462be613bfef98534c0107
durasno <sashka.warner@gmail.com>
2019-03-30 17:25:28 -0700

    add object

--- chapter5.js
+++ chapter5.js
@@ -1,6 +1,22 @@
-let arrayOfStrings, arrayOfNumbers, arrayMixed;
+/*let arrayOfStrings, arrayOfNumbers, arrayMixed;
 arrayOfStrings = ["a", "b", "c"];
 arrayOfNumbers = [1, 2, 3];
-arrayMixed = ["a", 1, null, true, arrayOfNumbers, [4.5, 5.6]];
+arrayMixed = ["a", 1, null, true, arrayOfNumbers, [4.5, 5.6]];*/
 
-$("#response").html(arrayOfStrings[2]);
+//$("#response").html(arrayOfStrings[2]);
+
+let myBurritoObject;
+myBurritoObject = {
+  tortilla: "wheat",
+  guacamole: true,
+  beans: "pinto",
+  habaneroSauceSquirts: 3
+  };
+// and let’s access a property
+//$("#response").html(myBurritoObject["tortilla"]);
+
+$("#response").html(myBurritoObject.tortilla);
+
+let arrayOfStrings;
+arrayOfStrings = ["a", "b", "c"];
+$("#response").html(arrayOfStrings.length);
