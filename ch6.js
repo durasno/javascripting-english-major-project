261568731a4d016d83b89241184a7b22cc1ab740
durasno <sashka.warner@gmail.com>
2019-05-14 23:19:23 -0700

    fixed return statement

--- ch6.js
+++ ch6.js
@@ -1,6 +1,6 @@
 let makeFullName, hughessFullName;
 makeFullName = function(firstName, lastName){
-    firstName + " " + lastName;
-}
+  return (firstName + " " + lastName);
+};
 hughessFullName = makeFullName("Langston", "Hughes");
 console.log("Is your name " + hughessFullName + "?");
---
+++ ch6pt2.js
@@ -0,0 +1,18 @@
+let userString, upperCaseMinusE, upperCasedString;
+userString = prompt("What do you want to UPPeRCASe?");
+upperCaseMinusE = function(string){
+  let result;
+  result = "";
+  for (let i = 0; i < string.length; i = i + 1) {
+    let letter;
+    letter = string[i];
+    if ( letter === "e" ) {
+      result += letter;
+    } else {
+      result += letter.toUpperCase();
+    }
+  }
+  return result;
+};
+upperCasedString = upperCaseMinusE(userString);
+$("#response").html(upperCasedString);
