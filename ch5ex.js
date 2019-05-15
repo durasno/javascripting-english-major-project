d925a9e6cb89dbea2a99116cc78dbcc115500ae6
durasno <sashka.warner@gmail.com>
2019-05-14 23:07:24 -0700

    added makeFullName function

--- ch5ex.js
+++ ch5ex.js
@@ -1,26 +1,30 @@
-/*let myArray = [];
-for(let i = 1; i < 4; i = i + i){
+let myArray = [];
+for(let i = 1; i < 4; i = i + 1){
   let userInput = prompt("Enter a number", "Enter the number here");
+  //$("#response").append(i);
   myArray[i] = userInput;
-  if(i === 2){
+  //$("#response").html(userInput);
+  if(i === 3){
     //return myArray;
-    $("#response").html(myArray[2]);
+    //$("#response").html(myArray[3]);
+    $("#response").html(myArray[i]);
   }
-}*/
+}
 
 /*array = ["1", "test", "I", "think", "maybe"];
 len = array.length;
 $("#response").html(array[len - 1])*/
 
-let ui = prompt("Enter a number", "Here");
+/*let ui = prompt("Enter a number", "Here");
 let un = Number(ui);
 a = typeof un;
 //$("#response").html(a);
 //$("#response").html(Number.isInteger(un));
 
+array =
 if(Number.isInteger(un)){
   $("#response").html("integer")
 }
 else{
   $("#response").html("not an integer")
-}
+}*/
