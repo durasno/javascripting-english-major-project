4c65b0a27ab2db0319394c884e73e88d12e366d5
durasno <sashka.warner@gmail.com>
2020-07-11 18:31:52 -0700

    added filter, map and ex 1 and 2

--- ch6pt2.js
+++ ch6pt2.js
@@ -1,6 +1,6 @@
 let userString, upperCaseMinusE, upperCasedString;
-userString = prompt("What do you want to UPPeRCASe?");
-upperCaseMinusE = function(string){
+/*userString = prompt("What do you want to UPPeRCASe?");*/
+/*upperCaseMinusE = function(string){
   let result;
   result = "";
   for (let i = 0; i < string.length; i = i + 1) {
@@ -13,6 +13,80 @@ upperCaseMinusE = function(string){
     }
   }
   return result;
+};*/
+upperCaseMinusE = function(string){
+  let result, stringArray;
+  result = "";
+  // Since forEach() only works on arrays, we have
+  // to convert the string to an array:
+  stringArray = string.split("");
+  // Now we call forEach() on stringArray:
+  stringArray.forEach(function(letter){
+    if ( letter === "e" ) {
+      result = result + letter;
+    } else {
+      result = result + letter.toUpperCase();
+    }
+  }) // Note the parenthesis!
+  return result;
 };
-upperCasedString = upperCaseMinusE(userString);
-$("#response").html(upperCasedString);
+
+let leonardo, donatello, raphael, michelangelo, turtles;
+leonardo = {name: "Leonardo", color: "blue", weapon: "katana"};
+donatello = {name: "Donatello", color: "purple", weapon: "bo"};
+raphael = {name: "Raphael", color: "red", weapon: "sai"};
+michelangelo = {name: "Michelangelo", color: "blue", weapon: "nunchaku"};
+turtles = [leonardo, donatello, raphael, michelangelo];
+
+weapons = ""; // a list of weapons.
+
+/*turtles.forEach(function(turtle){
+  weapons = weapons + turtle.weapon + " ";
+})*/
+
+weapons = turtles.map(function(turtle){
+  return turtle.weapon;
+}).sort();
+
+/*let names;
+names = turtles.map(function(turtle){
+  return turtle.name;
+}).sort();
+
+namesWithO = names.filter(function(name){
+  return name.includes("o");
+}).join(", ");*/
+
+//ex1
+/*array = [1,2,3,4];
+doubleFunc = function(inArr){
+  outArr = inArr.map(function(val){
+    return(2*val);
+  });
+  return outArr.join(", ");
+}
+$("#response").html(doubleFunc(array));*/
+
+//ex2
+iterFunc = function(array){
+  let result = 2;
+  result = "";
+  for(let i = 0; i < array.length; i = i + 1){
+    if(i === array.length-1){
+      result = result + "and " + array[i];
+    }
+    else{
+      result = result + array[i] + ", ";
+    }
+  }
+  return result;
+}
+
+$("#response").html(iterFunc(weapons));
+
+//$("#response").html(weapons);
+//$("#response").html(namesWithO);
+
+
+/*upperCasedString = upperCaseMinusE(userString);
+$("#response").html(upperCasedString);*/
