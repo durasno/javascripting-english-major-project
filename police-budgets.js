9ba46a24960b8248707aeb7b92c66ab84ac1e512
durasno <sashka.warner@gmail.com>
2020-07-27 23:24:36 -0700

    added markdown

--- police-budgets.js
+++ police-budgets.js
@@ -1,3 +1,21 @@
+// Define and assign a Markdown-it renderer.
+let md;
+md = window.markdownit({html: true}).use(window.markdownitFootnote);
+// Load the Markdown file with jQuery.
+/*$.ajax({
+  url: "https://the-javascripting-english-major.org/v1/examples/markdown/hastings-street.md",
+  success: function(markdown){*/
+  $.ajax({
+    url: "https://raw.githubusercontent.com/durasno/javascripting-english-major-project/master/data/police-budgets.md",
+    success: function(markdown){
+    // Convert the Markdown to HTML.
+    let html;
+    html = md.render(markdown);
+    // Print the HTML to #content using jQuery.
+    $("#content").html(html); //code wasn't working because content wasn't defined in the html sample
+  }
+});
+
 //let geocodeOut;
 let budgetMap, budgetTileLayer;
 budgetMap = L.map("police-budgets-map");
